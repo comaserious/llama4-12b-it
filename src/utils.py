93c4743ac11606d85d32c01b3b34c9f41e398b87
DESKTOP-V2RBQEJ\COM-NMOIM <sciboviator@gmail.com>
2026-02-05 16:26:50 +0900

    모듈 에러

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ from vllm.entrypoints.openai.protocol import RequestResponseMetadata
 
 try:
     from vllm.utils import random_uuid
-    from vllm.entrypoints.openai.protocol import ErrorResponse
+    from vllm.entrypoints.openai.engine.protocol import ErrorResponse
     from vllm import SamplingParams
 except ImportError:
     logging.warning("Error importing vllm, skipping related imports. This is ONLY expected when baking model into docker image from a machine without GPUs")
